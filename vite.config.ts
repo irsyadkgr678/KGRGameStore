d03489064898d79640d38ea01a131f190476794b
Muhammad Irsyad <136970971+irsyadkgr678@users.noreply.github.com>
2025-06-16 00:45:23 +0700

    Revert vite.config.ts to default configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: '.',
-  build: {
-    rollupOptions: {
-      input: 'index.html'
-    }
-  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
